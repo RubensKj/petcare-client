c601a4d3ba5bc2120f13190594e754ec94d87dd6
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-08-20 16:58:26 -0300

    Adding list of companies

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -10,14 +10,14 @@ export default function Main() {
     <>
       <HeaderMainPage />
       <div className="container-main">
-        <div className="list-petshops">
-          <CompanyCard text="Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker." />
-          <CompanyCard />
-          <CompanyCard />
-          <CompanyCard />
-          <CompanyCard />
-          <CompanyCard />
-          <CompanyCard />
+        <div className="petshops">
+          <div className="header-list-petshops">
+            <h2>Pet shops</h2>
+          </div>
+          <div className="list-petshops">
+            <CompanyCard text="Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker." />
+            <CompanyCard />
+          </div>
         </div>
       </div>
     </>
