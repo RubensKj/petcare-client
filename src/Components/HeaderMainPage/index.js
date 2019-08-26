25409c62b69d791fe730de2c9d723c7fe0e125f2
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-08-26 15:58:10 -0300

    Finishing Header

--- src/Components/HeaderMainPage/index.js
+++ src/Components/HeaderMainPage/index.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Logo from "../../Assets/PetCareLogo";
+
+import { isAuthenticated } from '../../Services/auth';
 
 import "./styles.css";
 
 export default function HeaderMainPage({ hideBtns }) {
+    const [isAuth, setIsAuth] = useState(false);
+
+    useEffect(() => {
+        if (isAuthenticated()) {
+            console.log("Authenticado")
+            setIsAuth(true);
+        }
+    }, [])
+
     return (
         <header className="header-main-container">
             <div className="content-header">
@@ -12,12 +23,25 @@ export default function HeaderMainPage({ hideBtns }) {
                     <Logo />
                 </div>
                 <nav className="nav-header-main">
-                    <a href="/entrar" className="entrar-a">
-                        <div className="btn-entrar">
-                            <span>Entrar</span>
-                        </div>
-                    </a>
-                    <a href="/cadastrar" className="cadastrar-btn">Cadastre seu pet shop</a>
+                    {isAuth ? (
+                        <>
+                            <div className="actions-header">
+                                <a href="/favoritos"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="square" strokeLinejoin="arcs"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></a>
+                                <a href="/sacola"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="square" strokeLinejoin="arcs"><path d="M6 2L3 6v14c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2V6l-3-4H6zM3.8 6h16.4M16 10a4 4 0 1 1-8 0"/></svg></a>
+                                <a href="/profile"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="square" strokeLinejoin="arcs"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg></a>
+                                <a href="/login"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" strokeWidth="2" strokeLinecap="square" strokeLinejoin="arcs"><path d="M15 3h6v18h-6M10 17l5-5-5-5M13.8 12H3" /></svg></a>
+                            </div>
+                        </>
+                    ) : (
+                            <>
+                                <a href="/entrar" className="entrar-a">
+                                    <div className="btn-entrar">
+                                        <span>Entrar</span>
+                                    </div>
+                                </a>
+                                <a href="/cadastrar" className="cadastrar-btn">Cadastre seu pet shop</a>
+                            </>
+                        )}
                 </nav>
             </div>
         </header>
