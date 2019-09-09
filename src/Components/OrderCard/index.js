909002265e19323149a1ad0d1f7a88ce4df51cb2
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-09-09 14:00:20 -0300

    Adding loading and fixing order card

--- src/Components/OrderCard/index.js
+++ src/Components/OrderCard/index.js
@@ -9,14 +9,15 @@ export default function OrderCard({ order, finished }) {
   return (
     <a href={`/pedidos/${order.id}`} className={finished ? ('order-card finished') : ('order-card')}>
       <div className="content-order">
-        <div className="image-card">
-          <img src="https://scontent.fcwb2-1.fna.fbcdn.net/v/t1.0-9/36919020_268531707232126_6615945512266760192_n.jpg?_nc_cat=104&_nc_oc=AQko23jqhSEdfdI0hGPYOHjdTZOoXcvXCYhUNdiqWIcHM3i7zdB2eLqlqzE4EdhKErc&_nc_ht=scontent.fcwb2-1.fna&oh=ed883d3681026683bcadcbc8a4d909b0&oe=5E134CF4" alt="Company Logo Order Card" />
-        </div>
         <div className="card-information-order">
           <div className="header-card">
             <div className="title-card-order">
               <h1>Pedido:</h1>
               <h1 className="number-order">#{order.id}</h1>
+            </div>
+            <div className="company-area-name">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#394458" strokeWidth="2" strokeLinecap="square" strokeLinejoin="arcs"><path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"></path><line x1="7" y1="7" x2="7" y2="7"></line></svg>
+              <data>{order.nameCompany}</data>
             </div>
             <div className="date-of-order">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#394458" strokeWidth="2" strokeLinecap="square" strokeLinejoin="arcs"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
@@ -27,12 +28,6 @@ export default function OrderCard({ order, finished }) {
             <div className="address-order">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#394458" strokeWidth="2" strokeLinecap="square" strokeLinejoin="arcs"><path d="M12 22s-8-4.5-8-11.8A8 8 0 0 1 12 2a8 8 0 0 1 8 8.2c0 7.3-8 11.8-8 11.8z" /><circle cx="12" cy="10" r="3" /></svg>
               <span>{address.street + ', ' + address.placeNumber + (address.complement ? (' - ' + address.complement) : (''))}</span>
-            </div>
-          </div>
-          <div className="footer-card">
-            <div className="price-card">
-              <span>R$</span>
-              <span className="price-number">{order.total}</span>
             </div>
           </div>
         </div>
--- src/Pages/Favorites/index.js
+++ src/Pages/Favorites/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import HeaderMainPage from '../../Components/HeaderMainPage';
+import Loading from '../../Components/Loading';
 import CompanyCard from '../../Components/CompanyCard';
 import EmptyContent from '../../Components/EmptyContent';
 import Subtitle from '../../Components/Subtitle';
@@ -10,11 +11,13 @@ import api from '../../Services/api';
 import './styles.css';
 
 export default function Favorites(props) {
+  const [isLoading, setIsLoading] = useState(true);
   const [favoritesCompanies, setFavoritesCompanies] = useState([]);
 
   async function loadFavorites(page) {
     await api.get(`/users/favorites-list/${page}`).then(res => {
       setFavoritesCompanies(res.data.content);
+      setIsLoading(false);
     });
   }
 
@@ -29,17 +32,21 @@ export default function Favorites(props) {
         <div className="content-favorites">
           <div className="content-list-favorites">
             <Subtitle text="Meus favoritos" />
-            {favoritesCompanies ? (
+            {isLoading ? (<Loading />) : (
               <>
-                <div className="list-favorites">
-                  {favoritesCompanies.map(company => <CompanyCard key={company.id} company={company} />)}
-                </div>
+                {favoritesCompanies ? (
+                  <>
+                    <div className="list-favorites">
+                      {favoritesCompanies.map(company => <CompanyCard key={company.id} company={company} />)}
+                    </div>
+                  </>
+                ) : (
+                    <>
+                      <EmptyContent title="Sua lista de favoritos!" description="Favorite um pet shop para que ele apareça aqui." svg={<svg xmlns="http://www.w3.org/2000/svg" width="78" height="78" viewBox="0 0 24 24" fill="#ebeced" stroke="#ebeced" strokeWidth="2" strokeLinecap="square" strokeLinejoin="arcs"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>} />
+                    </>
+                  )}
               </>
-            ) : (
-                <>
-                  <EmptyContent title="Sua lista de favoritos!" description="Favorite um pet shop para que ele apareça aqui." svg={<svg xmlns="http://www.w3.org/2000/svg" width="78" height="78" viewBox="0 0 24 24" fill="#ebeced" stroke="#ebeced" strokeWidth="2" strokeLinecap="square" strokeLinejoin="arcs"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>} />
-                </>
-              )}
+            )}
           </div>
         </div>
       </div>
