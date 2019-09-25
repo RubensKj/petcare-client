94e4262571b81f988d30a0ec886a100d4c7c813b
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-09-25 19:09:12 -0300

    Adding in gitignore

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { getToken } from "./auth";
 
 const api = axios.create({
-  // baseURL: 'http://192.168.25.17:9000/api',
   baseURL: process.env.REACT_APP_URL_API,
   headers: { 'Content-Type': 'application/json' },
 });
