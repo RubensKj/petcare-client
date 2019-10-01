3698c9960f6887df795846efb066dae30e6db4c7
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-10-01 20:20:15 -0300

    Fixing url to deploy

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,13 +2,8 @@ import axios from 'axios';
 import { getToken } from "./auth";
 
 const api = axios.create({
-<<<<<<< HEAD
-  baseURL: 'https://aw-petcare-api.herokuapp.com/api',
-  // baseURL: process.env.REACT_APP_API_URL,
-=======
   // baseURL: 'https://aw-petcare-api.herokuapp.com/api',
   baseURL: process.env.REACT_APP_API_URL,
->>>>>>> f0d70fdbfe30ceae5949e438e89e858698186304
   headers: { 'Content-Type': 'application/json' },
 });
 
