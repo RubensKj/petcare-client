c86159be97d2bf0e4b8b04d9141f6dd8c473fa1e
RubensKj <rubenskleinschmidtjr@gmail.com>
2019-10-01 20:24:20 -0300

    Fixing urls and commenting

--- src/Services/api.js
+++ src/Services/api.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 import { getToken } from "./auth";
 
 const api = axios.create({
+  // Local Host
+  // baseURL: 'http://192.168.25.17:9000/api',
+  // API Host
   // baseURL: 'https://aw-petcare-api.herokuapp.com/api',
+  // Deployed Host
   baseURL: process.env.REACT_APP_API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
